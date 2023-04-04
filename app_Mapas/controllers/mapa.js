a882d4dd6e6897a3d1ef450b2d0cdbbc52743139
Cavalex <mateusmpta@gmail.com>
2023-04-04 20:23:56 +0100

    made functional page

--- app_Mapas/controllers/mapa.js
+++ app_Mapas/controllers/mapa.js
@@ -1,11 +1,51 @@
 var Mapa = require('../models/mapa')
 
-// Student list
+// Mapa list
 module.exports.list = () => {
     return Mapa.find()
-            .sort({nome:-1})
+        .sort({nome:-1})
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.getMapa = id => {
+    return Mapa.findOne({_id: id})
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.addAluno = a => {
+    return Mapa.create(a)
+            .then(dados => {
+                return dados
+            })
+            .catch(erro => {
+                return erro
+            })
+}
+
+module.exports.updateAluno = a => {
+    return Mapa.updateOne({_id: a._id}, a)
             .then(resposta => {
                 return resposta
+            })
+            .catch(erro => {
+                return erro
+            })
+}
+
+module.exports.deleteAluno = id => {
+    return Mapa.deleteOne({_id: id})
+            .then(resposta => {
+                return resposta.data
             })
             .catch(erro => {
                 return erro
--- app_Mapas/routes/index.js
+++ app_Mapas/routes/index.js
@@ -5,8 +5,15 @@ var Mapa = require('../controllers/mapa')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Mapa.list()
-  .then(dados=> res.status(200).json(dados))
-  .catch(erro =>res.status(520).json({erro:erro,mesagem:"erro na obtençao de lista"}))
+    .then(mapas => {
+      res.render('index', { slist: mapas});
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção da lista"})
+    })
+
+    /* .then(dados=> res.status(200).json(dados))
+    .catch(erro =>res.status(520).json({erro:erro,mesagem:"erro na obtençao de lista"})) */
 });
 
 module.exports = router;
