cbbfdfd6c1bef70c7b1aab212ccda07e41dadc34
joaocasr <castrojoao839@gmail.com>
2023-04-15 13:22:43 +0100

    alteracao das queries de lugares,datas e entidades relacionados

--- app_Mapas/controllers/mapa.js
+++ app_Mapas/controllers/mapa.js
@@ -54,7 +54,8 @@ module.exports.deleteRua = id => {
 }
 
 module.exports.getRelatedLugares = lugar => {
-    return Mapa.find({"para": {"$elemMatch": {"lugar": {"$in": [lugar]}}}})
+    return Mapa.find({"$or":[{"para": {"$elemMatch": {"lugar": {"$in": [lugar]}}}},
+    {"listacasas": {"$elemMatch": {"desc.para.lugar": {"$in": [lugar]}}}}]})
             .then(streets => {
                 return streets
             })
@@ -64,7 +65,8 @@ module.exports.getRelatedLugares = lugar => {
 }
 
 module.exports.getRelatedDatas= data => {
-    return Mapa.find({"para": {"$elemMatch": {"data": {"$in": [data]}}}})
+    return Mapa.find({"$or":[{"para": {"$elemMatch": {"data": {"$in": [data]}}}},
+    {"listacasas": {"$elemMatch": {"desc.para.data": {"$in": [data]}}}}]})
             .then(streets => {
                 return streets
             })
@@ -74,7 +76,8 @@ module.exports.getRelatedDatas= data => {
 }
 
 module.exports.getRelatedEntidades= entity => {
-    return Mapa.find({"para": {"$elemMatch": {"entidade": {"$elemMatch": {"text":entity}}}}})
+    return Mapa.find({"$or":[{"listacasas": {"$elemMatch": {"desc.para.entidade": {"$elemMatch": {"text":entity}}}}},
+    {"para": {"$elemMatch": {"entidade": {"$elemMatch": {"text":entity}}}}}]})
             .then(streets => {
                 return streets
             })
