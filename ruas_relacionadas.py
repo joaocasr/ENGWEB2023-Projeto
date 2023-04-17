9ce21a96d16041a54024d3ff072c37c0417b54da
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-04-17 13:40:25 +0100

    f

--- ruas_relacionadas.py
+++ ruas_relacionadas.py
@@ -14,11 +14,12 @@ def inf_relaçao(estradas):
                 if 'data' in estrada['para'][p]:
                     for d in estrada['para'][p]['data']:
                         data.append(d)
-                if 'entidades' in estrada['para'][p]:
-                    for e in estrada['para'][p]['entidades']:
-                        lugares.append(e)
+                if 'entidade' in estrada['para'][p]:
+                    for e in estrada['para'][p]['entidade']:
+                        entidades.append(e)
         estrada={'_id':estrada['_id'],'lugares':lugares,'data':data,'entidades':entidades}
         new_estradas.append(estrada)
+
     return new_estradas
 
 def estradas_rel(estradas):
@@ -29,14 +30,27 @@ def estradas_rel(estradas):
 
     for i in range(len(estradas)):
         for j in range(i,len(estradas)):
-            if 'lugares' in estradas[i] and 'lugares' in estradas[j] :
+            if 'lugares' in estradas[i] and 'lugares' in estradas[j] and i != j:
                 for lugar in estradas[i]['lugares'] :
                     if lugar in estradas[j]['lugares']:
-                        estradas_relacionadas[i]['lugares'].append(j)
-                        estradas_relacionadas[j]['lugares'].append(i)
+                        estradas_relacionadas[i]['lugares'].append(estradas_relacionadas[j]['_id'])
+                        estradas_relacionadas[j]['lugares'].append(estradas_relacionadas[i]['_id'])
                         break
+            if 'data' in estradas[i] and 'data' in estradas[j] and i != j:
+                for data in estradas[i]['data'] :
+                    if data in estradas[j]['data']:
+                        estradas_relacionadas[i]['data'].append(estradas_relacionadas[j]['_id'])
+                        estradas_relacionadas[j]['data'].append(estradas_relacionadas[i]['_id'])
+                        break
+            if 'entidades' in estradas[i] and 'entidades' in estradas[j] and i != j:
+                for entidade_i in estradas[i]['entidades'] :
+                    for entidade_j in estradas[j]['entidades'] :
+                        if entidade_i['text'] == entidade_j['text'] and entidade_i['tipo'] == entidade_j['tipo']:
+                            estradas_relacionadas[i]['entidades'].append(estradas_relacionadas[j]['_id'])
+                            estradas_relacionadas[j]['entidades'].append(estradas_relacionadas[i]['_id'])
+                            break
     
-    print(estradas_relacionadas)
+    #print(estradas_relacionadas)
 
 def main():
     json_file = open("streetdb.json", "r",encoding='utf-8')
