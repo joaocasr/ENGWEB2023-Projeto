689f7f3853d96006af4edb7e4808082ee8eb8252
joaocasr <castrojoao839@gmail.com>
2023-04-25 02:32:10 +0100

    more stuff

--- app_Mapas/app.js
+++ app_Mapas/app.js
@@ -1,7 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const session = require('express-session')
+
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
 
 var mongoose = require('mongoose')
 var mongoDB = 'mongodb://127.0.0.1/MapaBraga'
@@ -21,13 +27,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+var userModel = require('./models/users')
+passport.use(new LocalStrategy(userModel.authenticate()))
+passport.serializeUser(userModel.serializeUser())
+passport.deserializeUser(userModel.deserializeUser())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({resave:false,saveUninitialized: true,secret:'blabla'}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_Mapas/routes/index.js
+++ app_Mapas/routes/index.js
@@ -1,25 +1,82 @@
 var express = require('express');
 var fs = require('fs')
 var router = express.Router();
+var passport = require('passport')
 var Mapa = require('../controllers/mapa')
 var Related = require('../controllers/relations')
 
 
+function verificaAutenticacao(req, res, next){
+  console.log('User (verif.): ' + JSON.stringify(req.user))
+  if(req.isAuthenticated()){
+      //req.isAuthenticated() will return true if user is logged in
+      next();
+  } else{
+    res.redirect("/");
+  }
+}
+
+
+router.get('/', function(req, res) {
+  res.render('login')
+})
+  
+router.post('/', passport.authenticate('local'), function(req, res) {
+ 	res.redirect('/ruas')
+})
+
+router.get('/logout', function(req, res) {
+  req.logout(function(err){
+    if(err){
+      console.log("LOGOUT ERROR"+err)
+      res.render('error',{error:err})
+    }else{
+      res.redirect('/')
+    }
+  })
+})
+
+//Register
+router.get('/register',function(req,res){
+  res.render('register')
+})
+
+var userModel= require('../models/users')
+router.post('/register',function(req,res){
+  var d = new Date().toISOString().substring(0,16)
+  userModel.register(new userModel({
+    username:req.body.username,
+    email:req.body.email,
+    name:req.body.name,
+    dateCreated:d,
+    role:req.body.role,
+    active:true
+  }),req.body.password,function(err,user){
+    if(err){
+      console.log("register error "+err)
+      res.render('error',{error:err})
+    }else{
+      res.redirect('/')
+    }
+  })
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/ruas', function(req, res, next) {
+  if(req.isAuthenticated()){
   Mapa.list()
     .then(mapa => {
-      res.render('index', { streets: mapa});
-      //res.status(200).json(mapas)
+      res.render('index', { streets: mapa, user: req.user});
     })
     .catch(erro => {
       res.render('error', {error: erro, message: "Erro na obtenção da lista de ruas."})
     })
-    /* .then(dados=> res.status(200).json(dados))
-    .catch(erro =>res.status(520).json({erro:erro,mesagem:"erro na obtençao de lista"})) */
+  }else{
+    res.render('error', {error:"Área restrita" ,message: "<p>Para aceder ao conteúdo da plataforma 'Braga Street View', efetue o <a href=\"/\">login</a>.</p><p>Se ainda não possui uma conta registe-se <a href=\"/register\">aqui.</a></p>"})
+  }
 });
 
-router.get('/ruas/:idRua', function(req, res, next) {
+router.get('/ruas/:idRua', verificaAutenticacao ,function(req, res, next) {
   Mapa.getRua(req.params.idRua).then(rua => {
           Related.getRelatedStreets(rua._id).then(related => {
           console.log(related);
@@ -52,4 +109,5 @@ router.get('/delete/:id', function(req, res, next) {
   Mapa.deleteRua(req.params.id)
   res.redirect("/");
 });
+
 module.exports = router;
