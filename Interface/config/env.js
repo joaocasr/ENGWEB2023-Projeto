7e161abe41ca66e7fdd64f9253eb0125e40e9ea0
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-06-18 23:39:55 +0100

    Update index.js

--- Interface/config/env.js
+++ Interface/config/env.js
@@ -1,2 +1,2 @@
-module.exports.apiAccessPoint ="http://localhost:7777/api";
-module.exports.authAccessPoint = "http://localhost:7779/users"
+module.exports.apiAccessPoint =process.env.API_URL;
+module.exports.authAccessPoint = process.env.AUTH_URL
