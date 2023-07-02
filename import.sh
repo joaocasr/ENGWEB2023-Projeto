f3de6d806af62d5ee3d6e127206bf0cf0d37c3ed
joaocasr <castrojoao839@gmail.com>
2023-07-02 15:11:13 +0100

    script para importar datasets no container do mongo

--- import.sh
+++ import.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Build the Docker image
-docker build -t charming .
+# container name: charming_albattani with mongo image
+docker cp data/streetdb.json charming_albattani:tmp/streetdb.json
 
-# Run a container from the image
-docker run -p 8000:8000 charming
+docker cp data/relationsdb.json charming_albattani:tmp/relationsdb.json
+
+docker cp data/user.json charming_albattani:tmp/user.json
+
+docker cp data/comentarios.json charming_albattani:tmp/comentarios.json
+
+
+sudo docker exec charming_albattani mongoimport -d MapaBraga -c streets --file /tmp/streetdb.json --jsonArray
+
+sudo docker exec charming_albattani mongoimport -d MapaBraga -c relations --file /tmp/relationsdb.json --jsonArray
+
+sudo docker exec charming_albattani mongoimport -d MapaBraga -c users --file /tmp/user.json --jsonArray
+
+sudo docker exec charming_albattani mongoimport -d MapaBraga -c comentarios --file /tmp/comentarios.json --jsonArray
